867102cf916e161b843dcebbc4cd483171e0f297
wshuai <wangshuai@reapal.com>
2018-05-22 15:36:38 +0800

    请求注解拦截

--- Travel.Api.DTO/BackTicket/Request/BackTicketRequest.cs
+++ Travel.Api.DTO/BackTicket/Request/BackTicketRequest.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
 
-namespace Travel.Api.DTO.BackTicket.Request
-{
+namespace Travel.Api.DTO.BackTicket.Request {
     /// <summary>
     /// 放款回调请求实体
     /// </summary>
@@ -11,14 +9,17 @@ namespace Travel.Api.DTO.BackTicket.Request
         /// <summary>
         /// 订单号
         /// </summary>
+        [Required(ErrorMessage = "必须填写")]
         public string OrderId { get; set; }
         /// <summary>
         /// 放款状态
         /// </summary>
+        [Required(ErrorMessage = "必须填写")]
         public string BackStatus { get; set; }
         /// <summary>
         /// 放款时间
         /// </summary>
+        [Required(ErrorMessage = "必须填写")]
         public DateTime BackTime { get; set; }
         /// <summary>
         /// 放款失败原因
